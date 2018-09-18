efb25578d35015cd517b3a2332e50707e9d63869
kindsagacity <kindsagacity@gmail.com>
2018-09-18 19:04:44 +0300

    18-app-empty-booking-screen
    Home screen UI

---
+++ app/components/ui/ProfileButton/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native'
+
+import { colors } from 'theme'
+
+export default StyleSheet.create({
+  image: {
+    backgroundColor: colors.gray100,
+    width: 30,
+    height: 30,
+    borderRadius: 25
+  }
+})
