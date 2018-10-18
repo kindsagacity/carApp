87bb0b8d6fb08bb1d42cbe4d514da99f58e3520d
Unknown <ozariss15@gmail.com>
2018-10-19 02:35:47 +0300

    App
    -user react-native-image-picker instead of react-native-camera on Documentation and ProfileMain screens

--- app/components/screens/Profile/ProfileMain/index.js
+++ app/components/screens/Profile/ProfileMain/index.js
@@ -1,14 +1,18 @@
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
-import {signOut} from 'store/actions/auth'
+import {signOut, updateUserImage} from 'store/actions/auth'
+import { getUpdateError, getAuthStatus } from 'store/selectors/auth'
 import ProfileMain from './ProfileMain'
 import {getUserData} from 'store/selectors'
 
 const selector = createStructuredSelector({
-  user: getUserData
+  user: getUserData,
+  error: getUpdateError,
+  requestPending: getAuthStatus
 })
 
 const actions = {
+  onUpdateUserImage: updateUserImage,
   onSignOut: signOut
 }
 
