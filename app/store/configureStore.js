6f39f0bf9d562defae9dd493e3eb92063244b1c9
Unknown <ozariss15@gmail.com>
2018-10-29 00:09:49 +0200

    App
    use default redux-persist store instead of secured one

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -3,23 +3,23 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { persistStore, persistReducer } from 'redux-persist'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import createSensitiveStorage from 'redux-persist-sensitive-storage'
+// import createSensitiveStorage from 'redux-persist-sensitive-storage'
+import storage from 'redux-persist/lib/storage'
 export default (rootReducer, rootSaga) => {
   const middlewares = [
     thunk
   ]
   const enhancers = []
-  const sensitiveStorage = createSensitiveStorage({
-    keychainService: 'CarflowKeychain',
-    sharedPreferencesName: 'CarflowPrefs'
-  })
+  // const sensitiveStorage = createSensitiveStorage({
+  //   keychainService: 'CarflowKeychain',
+  //   sharedPreferencesName: 'CarflowPrefs'
+  // })
   const persistConfig = {
     key: 'root',
-    storage: sensitiveStorage,
+    storage,
     whitelist: ['auth']
   }
 
-  // const sagaMonitor = __DEV__ ? console.tron.createSagaMonitor() : null
   const sagaMonitor = null
   const sagaMiddleware = createSagaMiddleware({ sagaMonitor })
   middlewares.push(sagaMiddleware)
@@ -35,9 +35,6 @@ export default (rootReducer, rootSaga) => {
 
   sagaMiddleware.run(rootSaga)
 
-  // store = createStore(persistedReducer, composeEnhancers(
-  //   applyMiddleware(thunk)
-  // ))
   let persistor = persistStore(store)
   // persistor.purge()
 
