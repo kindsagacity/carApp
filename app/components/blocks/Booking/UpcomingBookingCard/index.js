aa504b7235660af546b581b436b71ae11f592bd2
Dominus Infernus <domin@host-192-168-88-189.soho.net.ua>
2018-10-30 19:32:18 +0200

    Task 141 * fix Upcoming screen's Count down

--- app/components/blocks/Booking/UpcomingBookingCard/index.js
+++ app/components/blocks/Booking/UpcomingBookingCard/index.js
@@ -1,23 +1,64 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import {BookingCard} from '../BookingCard'
 import moment from 'moment-timezone'
 import PropTypes from 'prop-types'
 
-const UpcomingBookingCard = ({booking, onPress}) => {
-  const onBookingPress = () => {
+class UpcomingBookingCard extends PureComponent {
+  constructor (props) {
+    super(props)
+
+    this.startingAt = moment.tz(props.booking['booking_starting_at'].object.date, 'America/New_York')
+
+    this.state = {
+      extraDetail: ''
+    }
+  }
+
+  componentDidMount () {
+    this.getTime()
+    this.timer = setInterval(this.getTime, 30000)
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.timer)
+  }
+
+  getTime = () => {
+    const now = moment().tz('America/New_York')
+
+    if (now.isAfter(this.startingAt)) {
+      clearInterval(this.timer)
+
+      this.setState({
+        extraDetail: 'You can unlock the car now'
+      })
+    } else {
+      this.setState({
+        extraDetail: `Starting in ${now.to(this.startingAt, true)}`
+      })
+    }
+  }
+
+  onBookingPress = () => {
+    const { onPress, booking } = this.props
+
     onPress(booking)
   }
-  let startingAt = moment.tz(booking['booking_starting_at'].object.date, 'America/New_York')
-  let now = moment().tz('America/New_York')
-  return (
-    <BookingCard
-      booking={booking.car}
-      bookingEnd={booking['booking_ending_at'].formatted}
-      bookingStart={booking['booking_starting_at'].formatted}
-      extraDetail={`Starting in ${now.to(startingAt, true)}`}
-      onPress={onBookingPress}
-    />
-  )
+
+  render () {
+    const { booking } = this.props
+    const { extraDetail } = this.state
+
+    return (
+      <BookingCard
+        booking={booking.car}
+        bookingEnd={booking['booking_ending_at'].formatted}
+        bookingStart={booking['booking_starting_at'].formatted}
+        extraDetail={extraDetail}
+        onPress={this.onBookingPress}
+      />
+    )
+  }
 }
 
 UpcomingBookingCard.propTypes = {
