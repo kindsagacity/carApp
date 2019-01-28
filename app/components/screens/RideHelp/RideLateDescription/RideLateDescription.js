190e1ab341009995ea7d158137dd9fc4edfdb9e5
Vitalii Obideiko <vitalii.obideiko@itomy.ch>
2019-01-28 18:01:44 +0200

    Fix issue with reporting I'm late (Submit report button)

--- app/components/screens/RideHelp/RideLateDescription/RideLateDescription.js
+++ app/components/screens/RideHelp/RideLateDescription/RideLateDescription.js
@@ -5,7 +5,8 @@ import {
   ScrollView,
   TouchableOpacity,
   Alert,
-  Platform
+  Platform,
+  StyleSheet
 } from 'react-native'
 import PropTypes from 'prop-types'
 import isEmpty from 'lodash/isEmpty'
@@ -23,8 +24,7 @@ import {
 } from 'components/ui'
 import { Spinner } from 'components/ui'
 import ImagePicker from 'react-native-image-picker'
-import { colors } from 'theme'
-import styles from './styles'
+import { colors, metrics } from 'theme'
 
 const validationSchema = Yup.object().shape({
   reason: Yup.string()
@@ -61,18 +61,26 @@ class RideLateDescription extends PureComponent {
   state = {
     delay: null
   }
+
   componentDidUpdate(prevProps) {
     const { error, requestPending, navigation } = this.props
+
     if (prevProps.requestPending && !requestPending) {
-      if (error) setTimeout(() => Alert.alert('Error', error), 200)
-      else navigation.navigate(HelpCenter)
+      if (error) {
+        setTimeout(() => Alert.alert('Error', error), 200)
+      } else {
+        navigation.navigate(HelpCenter)
+      }
     }
   }
+
   componentWillUnmount() {
     this.props.onResetPhotos('rideLatePhotos')
   }
+
   onPhotoPress = async () => {
     let granted = await requestMainPermissions(true)
+
     if (granted) {
       const { onSavePhoto } = this.props
       // onSelectPhoto({ type: 'rideLatePhotos', index: 0 })
@@ -82,25 +90,29 @@ class RideLateDescription extends PureComponent {
       ImagePicker.showImagePicker(
         Platform.OS === 'android' ? androidOptions : iosOptions,
         response => {
-          if (!response.didCancel || response.error)
+          if (!response.didCancel || response.error) {
             onSavePhoto({
               type: 'rideLatePhotos',
               index: 0,
               photoUri: response.uri
             })
+          }
         }
       )
     }
   }
+
   onSubmit = values => {
     const { reason } = values
     const { onSubmitReport, ride = {}, photos, notification } = this.props
+
     onSubmitReport({
       data: { photos, reason, delay: this.state.delay },
       carId: ride.id,
       notificationId: notification.id
     })
   }
+
   renderForm = ({
     setFieldTouched,
     handleChange,
@@ -110,15 +122,17 @@ class RideLateDescription extends PureComponent {
     touched
   }) => {
     const { reason } = values
-    let buttonActive = isEmpty(errors) && touched.reason && this.state.delay
+    let buttonActive = !!(isEmpty(errors) && reason && this.state.delay)
+
     return (
       <ScrollView
         contentContainerStyle={styles.container}
-        keyboardShouldPersistTaps="always"
+        keyboardShouldPersistTaps={'always'}
       >
         <View style={styles.form}>
           <View style={styles.delayContainer}>
-            <SectionHeader title="Delaying" />
+            <SectionHeader title={'Delaying'} />
+
             <View>
               <TouchableOpacity
                 style={styles.checkboxContainer}
@@ -126,48 +140,57 @@ class RideLateDescription extends PureComponent {
               >
                 <RadioButton
                   checked={this.state.delay === 15}
-                  checkedIcon="md-radio-button-on"
+                  checkedIcon={'md-radio-button-on'}
                   onPress={() => this.setState({ delay: 15 })}
                 />
-                <Text style={styles.checkboxTitle}>15 minutes</Text>
+
+                <Text style={styles.checkboxTitle}>{'15 minutes'}</Text>
               </TouchableOpacity>
+
               <TouchableOpacity
                 style={styles.checkboxContainer}
                 onPress={() => this.setState({ delay: 30 })}
               >
                 <RadioButton
                   checked={this.state.delay === 30}
-                  checkedIcon="md-radio-button-on"
+                  checkedIcon={'md-radio-button-on'}
                   onPress={() => this.setState({ delay: 30 })}
                 />
-                <Text style={styles.checkboxTitle}>30 minutes</Text>
+
+                <Text style={styles.checkboxTitle}>{'30 minutes'}</Text>
               </TouchableOpacity>
+
               <TouchableOpacity
                 style={styles.checkboxContainer}
                 onPress={() => this.setState({ delay: 45 })}
               >
                 <RadioButton
                   checked={this.state.delay === 45}
-                  checkedIcon="md-radio-button-on"
+                  checkedIcon={'md-radio-button-on'}
                   onPress={() => this.setState({ delay: 45 })}
                 />
-                <Text style={styles.checkboxTitle}>45 minutes</Text>
+
+                <Text style={styles.checkboxTitle}>{'45 minutes'}</Text>
               </TouchableOpacity>
+
               <TouchableOpacity
                 style={styles.checkboxContainer}
                 onPress={() => this.setState({ delay: 60 })}
               >
                 <RadioButton
                   checked={this.state.delay === 60}
-                  checkedIcon="md-radio-button-on"
+                  checkedIcon={'md-radio-button-on'}
                   onPress={() => this.setState({ delay: 60 })}
                 />
-                <Text style={styles.checkboxTitle}>1 hour</Text>
+
+                <Text style={styles.checkboxTitle}>{'1 hour'}</Text>
               </TouchableOpacity>
             </View>
           </View>
+
           <View style={styles.photoUploadContainer}>
-            <SectionHeader title="Upload photo" />
+            <SectionHeader title={'Upload photo'} />
+
             <View style={styles.photoContainer}>
               <Photo
                 imageUri={this.props.photos[0]}
@@ -175,24 +198,26 @@ class RideLateDescription extends PureComponent {
               />
             </View>
           </View>
+
           <TextInputView
             blurOnSubmit
             error={touched.reason && errors.reason}
-            keyboardType="default"
-            label="Reason"
+            keyboardType={'default'}
+            label={'Reason'}
             maxLength={1000}
             multiline
-            name="reason"
-            placeholder="Why are you late?"
+            name={'reason'}
+            placeholder={'Why are you late?'}
             showLimit
             value={reason}
             onBlur={() => setFieldTouched('reason')}
             onChangeText={handleChange('reason')}
           />
         </View>
+
         <Button
           disabled={!buttonActive}
-          title="SUBMIT REPORT"
+          title={'SUBMIT REPORT'}
           onPress={handleSubmit}
         />
       </ScrollView>
@@ -211,11 +236,13 @@ class RideLateDescription extends PureComponent {
           validationSchema={validationSchema}
           onSubmit={this.onSubmit}
         />
+
         <Spinner color={colors.red} visible={this.props.requestPending} />
       </HelpCenterSection>
     )
   }
 }
+
 RideLateDescription.propTypes = {
   error: PropTypes.string,
   navigation: PropTypes.object,
@@ -230,3 +257,37 @@ RideLateDescription.propTypes = {
 }
 
 export default RideLateDescription
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    backgroundColor: colors.white,
+    justifyContent: 'space-between'
+  },
+
+  form: {
+    paddingTop: metrics.contentMarginSmall
+  },
+
+  textInput: {
+    marginBottom: 0
+  },
+
+  photoUploadContainer: {
+    paddingTop: metrics.contentMargin,
+    paddingBottom: metrics.contentMargin
+  },
+  photoContainer: {
+    marginTop: metrics.contentMarginSmall
+  },
+  checkboxContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 16
+  },
+  checkboxTitle: {
+    fontSize: metrics.fontSizeBig,
+    fontFamily: 'SFProText-Regular',
+    color: colors.black
+  }
+})
