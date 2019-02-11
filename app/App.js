56fb32ebf8429d951a9069a7c6b586ae0e53d3a0
Oleg Gamega <gadfil12@gmail.com>
2019-02-11 21:07:02 +0300

    firebase cloud message ios, settings done, work in progress

--- app/App.js
+++ app/App.js
@@ -1,20 +1,34 @@
 import React from 'react'
-import { Root } from 'navigation/config'
+import {Root} from 'navigation/config'
 import NavigationService from 'navigation/NavigationService'
+import firebase from "react-native-firebase";
 
-// import firebase from "react-native-firebase"
+// todo: willl be removed, only for test
+const init = async () => {
+    const enabled = await
+        firebase.messaging().hasPermission();
+    if (enabled) {
+        // user has permissions
+        return true
+    } else {
+        try {
+            await
+                firebase.messaging().requestPermission();
+            // User has authorised
+        } catch (error) {
+            // User has rejected permissions
+            return false
+        }
+    }
 
-
-// firebase.database().ref('test').once('value', function(snapshot) {
-//     console.log('postElement', snapshot.val());
-// });
-
+}
+init();
 const App = (props) => (
-  <Root
-    ref={navigatorRef => {
-      NavigationService.setTopLevelNavigator(navigatorRef)
-    }}
-  />
+    <Root
+        ref={navigatorRef => {
+            NavigationService.setTopLevelNavigator(navigatorRef)
+        }}
+    />
 )
 
 // new changes
